852faf07c64a45affd885516d46f0326f8762f55
cafferti <saulkumven@gmail.com>
2023-08-14 07:33:55 +0100

    addedindentsg

--- do.js
+++ do.js
@@ -5,44 +5,44 @@ const todolist = [{name :`wash diner`,
                    duedate: `20-6-2023`}]
                   //the above is an array of objects
 
-displaytodolist();
+        displaytodolist();
 
-function displaytodolist(){
-    
-let todolistHtml = ``
+        function displaytodolist(){
+            
+        let todolistHtml = ``
 
-for(let i = 0; i < todolist.length; i++){
-        const todoobject = todolist[i];
-        const task = todoobject.name;
-        const duedate = todoobject.duedate;
-        const html = 
-              
-         `<div>${task}</div>
-          <div>${duedate}</div>
-          <button onclick = " todolist.splice(${i}, 1); displaytodolist(); " >Delete</button>
-        `;
-        
+        for(let i = 0; i < todolist.length; i++){
+                const todoobject = todolist[i];
+                const task = todoobject.name;
+                const duedate = todoobject.duedate;
+                const html = 
+                      
+                `<div>${task}</div>
+                  <div>${duedate}</div>
+                  <button onclick = " todolist.splice(${i}, 1); displaytodolist(); " >Delete</button>
+                `;
+                
         todolistHtml += html;
 
         localStorage.setItem(`todolist`, JSON.stringify())
-}
-    document.querySelector(`.js-todo`).innerHTML = todolistHtml
+        }
+        document.querySelector(`.js-todo`).innerHTML = todolistHtml
 
 
-};
+        };
 
 
 
- function addtodo(){
-    
-   const inpElement = document.querySelector(`.js-input`) 
-   const name =  inpElement.value;
-   const dateinp =  document.querySelector(`.js-duedate`);
-   const duedate = dateinp.value;
-   todolist.push({name: name,
-                 duedate: duedate
-             })
-   console.log(todolist)
-   inpElement.value =  ""
-   displaytodolist();
-};
+        function addtodo(){
+            
+          const inpElement = document.querySelector(`.js-input`) 
+          const name =  inpElement.value;
+          const dateinp =  document.querySelector(`.js-duedate`);
+          const duedate = dateinp.value;
+          todolist.push({name: name,
+                        duedate: duedate
+                    })
+          console.log(todolist)
+          inpElement.value =  ""
+          displaytodolist();
+        };
